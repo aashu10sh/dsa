e84e9d247137b2abc764466d27b43d71e428aeb9
Aashutosh Pudasaini <nepalidude3@gmail.com>
2023-12-26 22:12:46 +0545

    finished :stack

--- stack/c++/attempt_original.cpp
+++ stack/c++/attempt_original.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
-#define MAX 20
+#define MAX 10
 
 class Stack {
 private:
     int data[MAX];
-    int top = -1 ;
+    int top = 0; // index of top most element
 
 public:
     Stack() {}
 
     int push(int data){
-        std::cout << (this->top +1 > MAX ) << std::endl;
-        if ( (top + 2) > MAX ){
-            throw std::overflow_error("StackOverflow");
-        }
         std::cout << "Pushing " << data << " To the Stack" << std::endl;
+        this->data[this->top] = data;
         this->top = this->top + 1;
-        this->data[this->top ] = data;
         return data; 
     }
     void printTop(){
-        std::cout << "Top is : "<<this->top << std::endl;
+        std::cout << "Top index is : "<<this->top << std::endl;
     }
     int pop() {
-        if (this->top -1  < -1 ){
-            throw std::underflow_error("StackUnderflow");
-        }
-        int original = this->data[this->top];
+        int original = this->data[this->top - 1];
         std::cout << "Popping " << original << " From the Stack" << std::endl;
         this->data[this->top] = 0;
         this->top = this->top - 1 ;
@@ -34,14 +27,20 @@ public:
     }
     void printStack() {
         std::cout << "Printing Stack! "<< this->top  << std::endl;
-        for(int i = 0 ;i<=top ; ++i){
-            std::cout << "| " << data[top - i] << " |" <<std::endl;
+        for(int i = 0 ;i<top ; ++i){
+            std::cout << "| " << data[i] << " |" <<std::endl;
         }
+        std::cout << "Note : Stack is in opposite order" << std::endl;
     }
     bool isEmpty() { return this->top == -1; }
 };
 
 int main() { 
     Stack stack;
+    stack.push(1);
+    stack.printStack();
     stack.pop();
+    stack.push(21);
+    stack.push(17);
+    stack.printStack();
 }
